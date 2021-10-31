57176aee7ac77375c8ff61b51f0fbe9b24a2e06e
Ankit Singh <ankitsingh4154@gmail.com>
2021-10-31 19:08:24 +0530

    Value objects in db via @Embedded and @Embeddable

--- src/com/stegan/hibernate/dto/Key.java
+++ src/com/stegan/hibernate/dto/Key.java
@@ -1,11 +1,13 @@
 package com.stegan.hibernate.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class Key {
-	
+	@Column(name = "PUBLIC_KEY")
 	private String publicKey;
+	@Column(name = "PRIVATE_KEY")
 	private String privateKey;
 	public String getPublicKey() {
 		return publicKey;
